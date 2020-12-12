92536af2c317fa5ee9c47b003e7e7b92e5323c13
Koray Guney <koray.guney@hotmail.com>
2020-12-12 13:43:43 +0300

    added exceptions examples 6

---
+++ BilgeAdam-05/src/com/bilgeadam/exceptionchallenge2/InvalidOperationException.java
@@ -0,0 +1,20 @@
+package com.bilgeadam.exceptionchallenge2;
+
+public class InvalidOperationException extends Exception{
+
+    public InvalidOperationException(){
+
+    }
+
+    public InvalidOperationException(String message){
+        super(message);
+    }
+
+    public InvalidOperationException(Throwable throwable){
+        super(throwable);
+    }
+
+    public InvalidOperationException(String message, Throwable throwable){
+        super(message, throwable);
+    }
+}
