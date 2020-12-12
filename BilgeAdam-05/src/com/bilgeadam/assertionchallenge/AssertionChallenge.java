bd3d9f7cce6064eebea7f3331f0993374a65650f
Koray Guney <koray.guney@hotmail.com>
2020-12-12 14:15:44 +0300

    added assertion example 3

--- BilgeAdam-05/src/com/bilgeadam/assertionchallenge/AssertionChallenge.java
+++ BilgeAdam-05/src/com/bilgeadam/assertionchallenge/AssertionChallenge.java
@@ -3,12 +3,13 @@ package com.bilgeadam.assertionchallenge;
 public class AssertionChallenge {
 
     public static void main(String[] args) {
-        setTime(15,65,25);
+        setTime(15,45,25);
     }
 
 
     public static void setTime(int hour,int minute,int second){
         assert ((hour >= 0 && hour < 24) && (minute >= 0 && minute < 60) && (second >= 0 && second <60)) : "Time is wrongly set";
+        assert (minute < 30) : "TEST";
         System.out.printf("The time is %d:%d:%d ", hour,minute,second);
     }
 
