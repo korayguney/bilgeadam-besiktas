bee8474402c539134b95dc651f220fefdd90ace8
Koray Guney <koray.guney@hotmail.com>
2020-12-26 12:42:27 +0300

    fixed comparable interface example

--- BilgeAdam-07/src/com/bilgeadam/comparator/ComparableStudent.java
+++ BilgeAdam-07/src/com/bilgeadam/comparator/ComparableStudent.java
@@ -3,9 +3,12 @@ package com.bilgeadam.comparator;
 public class ComparableStudent implements Comparable<ComparableStudent>{
     private String name; private long id = 0; private double gpa = 0.0;
 
-    public ComparableStudent(String name, long id, double gpa){
-        // Additional code here
+    public ComparableStudent(String name, long id, double gpa) {
+        this.name = name;
+        this.id = id;
+        this.gpa = gpa;
     }
+
     public String getName(){ return this.name; }
     // Additional code here
 
@@ -18,10 +21,37 @@ public class ComparableStudent implements Comparable<ComparableStudent>{
         return gpa;
     }
 
-    public int compareTo(ComparableStudent s){
-        int result = this.name.compareTo(s.getName());
-        if (result > 0) { return 1; }
-        else if (result < 0){ return -1; }
-        else { return 0; }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "ComparableStudent{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", gpa=" + gpa +
+                '}';
+    }
+
+    @Override
+    public int compareTo(ComparableStudent that) {
+        if (this.getName() == null && that.getName() == null) {
+            return 0;
+        } else if (this.getName() == null) {
+            return -1;
+        } else if (that.getName() == null) {
+            return 1;
+        } else {
+            return this.getName().compareTo(that.getName());
+        }
     }
 }
--- BilgeAdam-07/src/com/bilgeadam/comparator/TestComparator.java
+++ BilgeAdam-07/src/com/bilgeadam/comparator/TestComparator.java
@@ -7,25 +7,30 @@ import java.util.List;
 
 public class TestComparator {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         List<ComparableStudent> studentList = new ArrayList<>(3);
         Comparator<ComparableStudent> sortName = new StudentSortName();
         Comparator<ComparableStudent> sortGpa = new StudentSortGpa();
 
-        ComparableStudent c1 = new ComparableStudent("Ali",323, 3.4);
-        ComparableStudent c2 = new ComparableStudent("Hasan",454, 3.2);
+        ComparableStudent c1 = new ComparableStudent("Ali", 323, 3.4);
+        ComparableStudent c2 = new ComparableStudent("Hasan", 454, 3.2);
+        ComparableStudent c3 = new ComparableStudent("Fatma", 654, 2.7);
+        ComparableStudent c4 = new ComparableStudent("Şaban", 432, 4.0);
         studentList.add(c1);
         studentList.add(c2);
-        studentList.add(c2);
+        studentList.add(c3);
+        studentList.add(c4);
 
+        System.out.println("Compare according to student name");
         Collections.sort(studentList, sortName);
-        for(ComparableStudent student:studentList){
-            System.out.println(student);
+        for (ComparableStudent student : studentList) {
+            System.out.println("\t" + student.getName());
         }
 
+        System.out.println("Compare according to student gpa");
         Collections.sort(studentList, sortGpa);
-        for(ComparableStudent student:studentList){
-            System.out.println(student);
+        for (ComparableStudent student : studentList) {
+            System.out.println("\t" + student.getName() + "-->" + student.getGpa());
         }
     }
 
