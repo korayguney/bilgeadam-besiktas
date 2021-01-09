34d1669ab2e8e11f31eaf0a14e064ebf941f7da0
Koray Guney <koray.guney@hotmail.com>
2021-01-09 13:01:23 +0300

    added stream challenge 1 without answers

---
+++ BilgeAdam-09/src/com/bilgeadam/streamchallenge1/StreamChallenge1.java
@@ -0,0 +1,24 @@
+package com.bilgeadam.streamchallenge1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamChallenge1 {
+
+    List<String> strList = Arrays.asList("abc", "", "bcd", "", "defg", "jk");
+    // Count the empty strings
+    // Count String with length more than 3
+    // Count number of String which startswith "a"
+    // Remove all empty Strings from List
+    // Create a List with String more than 2 characters
+
+    List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.","Canada");
+    // Convert String to Uppercase and join them using comma
+
+    List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+    // Create List of square of all distinct numbers
+
+    List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+    //Get count, min, max, sum, and average for numbers
+
+}
