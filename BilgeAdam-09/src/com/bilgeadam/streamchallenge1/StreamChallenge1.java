16551a908400cabd05294f76b4e5a31ede92837a
Koray Guney <koray.guney@hotmail.com>
2021-01-09 13:02:38 +0300

    added stream challenge 1 without answers

--- BilgeAdam-09/src/com/bilgeadam/streamchallenge1/StreamChallenge1.java
+++ BilgeAdam-09/src/com/bilgeadam/streamchallenge1/StreamChallenge1.java
@@ -4,21 +4,24 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StreamChallenge1 {
+    public static void main(String[] args) {
+        List<String> strList = Arrays.asList("abc", "", "bcd", "", "defg", "jk");
+        // Count the empty strings
 
-    List<String> strList = Arrays.asList("abc", "", "bcd", "", "defg", "jk");
-    // Count the empty strings
-    // Count String with length more than 3
-    // Count number of String which startswith "a"
-    // Remove all empty Strings from List
-    // Create a List with String more than 2 characters
+        // Count String with length more than 3
+        // Count number of String which startswith "a"
+        // Remove all empty Strings from List
+        // Create a List with String more than 2 characters
 
-    List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.","Canada");
-    // Convert String to Uppercase and join them using comma
+        List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.","Canada");
+        // Convert String to Uppercase and join them using comma
 
-    List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
-    // Create List of square of all distinct numbers
+        List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+        // Create List of square of all distinct numbers
 
-    List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
-    //Get count, min, max, sum, and average for numbers
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        //Get count, min, max, sum, and average for numbers
+    }
+
 
 }
