c479143379dabcdebb6299f948d22e8c60f93922
Koray Guney <koray.guney@hotmail.com>
2021-01-09 21:24:55 +0300

    added ThreadColor.java

---
+++ BilgeAdam-09/src/com/bilgeadam/threadcolor/ThreadColor.java
@@ -0,0 +1,27 @@
+package com.bilgeadam.threadcolor;
+
+public class ThreadColor {
+    public static final String ANSI_RESET = "\033[0m";
+    public static final String ANSI_BLACK = "\033[30m";
+    public static final String ANSI_RED = "\033[31m";
+    public static final String ANSI_GREEN = "\033[32m";
+    public static final String ANSI_YELLOW = "\033[33m";
+    public static final String ANSI_BLUE = "\033[34m";
+    public static final String ANSI_PURPLE = "\033[35m";
+    public static final String ANSI_CYAN = "\033[36m";
+    public static final String ANSI_WHITE = "\033[37m";
+    
+    public static void main(String[] args) {
+        System.out.println("\033[0m BLACK");
+        System.out.println("\033[31m RED");
+        System.out.println("\033[32m GREEN");
+        System.out.println("\033[33m YELLOW");
+        System.out.println("\033[34m BLUE");
+        System.out.println("\033[35m MAGENTA");
+        System.out.println("\033[36m CYAN");
+        System.out.println("\033[37m WHITE");
+        
+    }   
+    
+    
+}
