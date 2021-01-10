cf255791b3df41ed17f47174ba81273055af11fe
Koray Guney <koray.guney@hotmail.com>
2021-01-10 12:45:02 +0300

    added multithreading examples

---
+++ BilgeAdam-09/src/com/bilgeadam/thread1/ConsumerProducer.java
@@ -0,0 +1,81 @@
+package com.bilgeadam.thread1;
+
+import com.bilgeadam.threadcolor.ThreadColor;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class ConsumerProducer {
+    public static void main(String[] args) {
+        SharedData data = new SharedData();
+        data.intValue = 100;
+        data.flag = false;
+
+        Producer p = new Producer(data);
+        Consumer c = new Consumer(data);
+        Thread t = new Thread(c);
+
+        p.start();
+        t.start();
+    }
+
+
+}
+
+class Producer extends Thread {
+    SharedData data;
+
+    public Producer(SharedData data) {
+        this.data = data;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // SharedData.lock.lock();
+            for (int i = 0; i < 10; i++) {
+                if (data.flag == false)
+                    synchronized (data) {
+                        data.intValue++; // critical section
+                    }
+                System.out.println(ThreadColor.ANSI_BLUE + "Producer :" + data.intValue);
+            }
+        } finally {
+            // SharedData.lock.unlock();
+            data.flag = true;
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    SharedData data;
+
+    public Consumer(SharedData data) {
+        this.data = data;
+    }
+
+    @Override
+    public void run() {
+        try {
+            //SharedData.lock.lock();
+            for (int i = 0; i < 10; i++) {
+                if (data.flag == true)
+                    synchronized (data) {
+                        data.intValue--; // critical section
+                    }
+                System.out.println(ThreadColor.ANSI_PURPLE + "Consumer :" + data.intValue);
+            }
+        } finally {
+            //SharedData.lock.unlock();
+            data.flag = false;
+        }
+
+    }
+}
+
+class SharedData {
+    static Lock lock = new ReentrantLock();
+    int intValue;
+    boolean flag;
+}
---
+++ BilgeAdam-09/src/com/bilgeadam/thread1/MyFirstThread.java
@@ -0,0 +1,51 @@
+package com.bilgeadam.thread1;
+
+import com.bilgeadam.threadcolor.ThreadColor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MyFirstThread {
+
+    public static void main(String[] args) {
+        // extended from Thead class
+        MyCustomThread myFirstThread = new MyCustomThread();
+        myFirstThread.setName("Extended from thread --->");
+
+        // implemented from Runnable interface
+        MyRunnableThread myRunnableThread = new MyRunnableThread();
+        Thread mySecondThread = new Thread(myRunnableThread);
+        mySecondThread.setName("Implemented from Runnable --->");
+
+        myFirstThread.setPriority(Thread.MAX_PRIORITY);
+        mySecondThread.setPriority(Thread.MIN_PRIORITY);
+
+        myFirstThread.start();
+        mySecondThread.start();
+
+        ExecutorService exec = Executors.newFixedThreadPool(2);
+        exec.submit(myRunnableThread);
+        exec.submit(myFirstThread);
+        exec.shutdown();
+    }
+
+    static class MyCustomThread extends Thread{
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(ThreadColor.ANSI_GREEN + Thread.currentThread().getName() + " : " + i );
+            }
+        }
+    }
+
+    static class MyRunnableThread implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(ThreadColor.ANSI_PURPLE + Thread.currentThread().getName() + " : " + i );
+            }
+        }
+    }
+
+}
