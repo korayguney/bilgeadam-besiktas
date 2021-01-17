7f40d4ec311c1dde6a6cde0706f6c3e8f4baa75f
Koray Guney <koray.guney@hotmail.com>
2021-01-17 13:19:18 +0300

    added jdbc sql example 1

---
+++ BilgeAdam-10/src/com/bilgeadam/jdbc/JDBC_Select.java
@@ -0,0 +1,41 @@
+package com.bilgeadam.jdbc;
+
+import java.sql.*;
+
+public class JDBC_Select {
+
+    public static void main(String[] args) {
+
+        try {
+            // create connection
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bilgeadam_db",
+                    "root", "1234");
+
+            // create statement
+            Statement stmt = conn.createStatement();
+
+            // execute query
+            ResultSet rs = stmt.executeQuery("SELECT * FROM customer2;");
+
+            // process query result
+            while (rs.next()) {
+                System.out.println(
+                        "ID :" + rs.getInt("id") +
+                                "Name : " + rs.getString("name") +
+                                "Address : " + rs.getString("address")
+                );
+            }
+
+            // close connections
+            stmt.close();
+            conn.close();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+    }
+
+
+}
