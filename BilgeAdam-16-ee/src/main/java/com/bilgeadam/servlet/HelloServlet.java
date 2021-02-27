a64167b85cb27c464d7cb7634b636f4971e992ed
Koray Guney <koray.guney@hotmail.com>
2021-02-27 14:20:17 +0300

    added second java ee project week 16

---
+++ BilgeAdam-16-ee/src/main/java/com/bilgeadam/servlet/HelloServlet.java
@@ -0,0 +1,36 @@
+package com.bilgeadam.servlet;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello ";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + name + " " + surname + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+
+    public void destroy() {
+    }
+}
---
+++ BilgeAdam-16/src/com/bilgeadam/servlet/Hello.java
@@ -0,0 +1,23 @@
+package com.bilgeadam.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class Hello extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().append("<h2>Hello from First Servlet</h2>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
