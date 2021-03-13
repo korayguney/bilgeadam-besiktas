222a7197267f6e177b250bdc9eda8fb4a4f166c0
Koray Guney <koray.guney@hotmail.com>
2021-03-13 13:02:08 +0300

    added link project example 3 week 18

---
+++ BilgeAdam-17-linkproject/src/main/java/com/bilgeadam/controller/MyLinksServlet.java
@@ -0,0 +1,30 @@
+package com.bilgeadam.controller;
+
+import com.bilgeadam.dao.LinkDAO;
+import com.bilgeadam.models.UserLink;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/mylinks")
+public class MyLinksServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = (String) req.getSession().getAttribute("username");
+
+        List<UserLink> userLinkList = new LinkDAO().getSelectedUserLinks(username);
+
+        req.setAttribute("links",userLinkList);
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
