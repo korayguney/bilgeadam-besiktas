a3bd64bd05e69c072d8e70bbc3f3a4b513be415e
Koray Guney <koray.guney@hotmail.com>
2021-04-18 14:09:58 +0300

    added spring boot week 6 23

---
+++ BilgeAdam-23/src/main/java/com/bilgeadam/bilgeadam23/Bilgeadam23Application.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.bilgeadam23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bilgeadam23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bilgeadam23Application.class, args);
+    }
+
+}
---
+++ BilgeAdam-23/src/test/java/com/bilgeadam/bilgeadam23/Bilgeadam23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.bilgeadam23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bilgeadam23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
