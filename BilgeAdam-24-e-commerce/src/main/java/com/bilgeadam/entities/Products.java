57be96529ee5684df9a8b896693e8d8f74b651ad
Koray Guney <koray.guney@hotmail.com>
2021-04-24 12:46:14 +0300

    added spring boot e-commerce example week 24

--- BilgeAdam-24-e-commerce/src/main/java/com/bilgeadam/entities/Products.java
+++ BilgeAdam-24-e-commerce/src/main/java/com/bilgeadam/entities/Products.java
@@ -12,9 +12,7 @@ import java.math.BigInteger;
 @NoArgsConstructor
 @Entity
 @Table(name = "products")
-
 public class Products extends AbstractEntity{
-    private BigInteger id;
     private String description;
     private String name;
     private BigDecimal price;
@@ -22,8 +20,7 @@ public class Products extends AbstractEntity{
     private String status;
     private BigInteger category_id;
 
-    public Products(BigInteger id, String description, String name, BigDecimal price, int sales_counter, String status, BigInteger category_id) {
-        this.id = id;
+    public Products(String description, String name, BigDecimal price, int sales_counter, String status, BigInteger category_id) {
         this.description = description;
         this.name = name;
         this.price = price;
