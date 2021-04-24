f7e2f22e36501118a82857330fa92cb499b2c846
furkan okcu <archerfurkan1@gmail.com>
2021-04-24 12:26:40 +0300

    Category and Review added

--- BilgeAdam-24-e-commerce/src/main/java/com/bilgeadam/repository/CategoryRepository.java
+++ BilgeAdam-24-e-commerce/src/main/java/com/bilgeadam/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.bilgeadam.repository;
 
 import com.bilgeadam.entities.Category;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends CrudRepository<Category,Long> {
 }
